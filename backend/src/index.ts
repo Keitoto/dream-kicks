0da6ccb7f2981d760eec2c2e1a3e378f849492da
Keitoto <keitasekihara@gmail.com>
2023-05-12 19:08:29 +0900

    🐛 Folder structure change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,14 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 
+import { products } from './data';
+
+const app = express();
+const PORT = 5000;
+
+app.get('/api/products', (req: Request, res: Response) => {
+  res.json(products);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+} );
