a9ff4119ffb82368965925439773a8235d39981d
Keitoto <keitasekihara@gmail.com>
2023-05-12 16:42:20 +0900

    🎉 Added dummy data

---
+++ src/types/Product.ts
@@ -0,0 +1,13 @@
+export type Product = {
+  id: string;
+  name: string;
+  brand: string;
+  category: string;
+  description: string;
+  image: string;
+  numInStock: number;
+  numReviews: number;
+  price: number;
+  rating: number;
+  slug: string;
+};
