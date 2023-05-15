0c393c406a6121067d14a120f19e6070b88568b0
Keitoto <keitasekihara@gmail.com>
2023-05-15 14:36:32 +0900

    ✨ Added Redux Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,16 +8,19 @@ import { router } from '@/pages/router';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from '@/store';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <HelmetProvider>
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <ReduxProvider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ReduxProvider>
     </MantineProvider>
   </HelmetProvider>
 );
