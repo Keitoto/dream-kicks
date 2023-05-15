d93e016af0b57d3dc05c3813130bb9ad0d14f5f7
Keitoto <keitasekihara@gmail.com>
2023-05-15 19:08:22 +0900

    ✏️ Fixed typo

--- frontend/src/components/Product/ProductList.tsx
+++ frontend/src/components/Product/ProductList.tsx
@@ -1,5 +1,6 @@
-import { SimpleGrid } from '@mantine/core';
 import { FC } from 'react';
+import { SimpleGrid } from '@mantine/core';
+
 import ProductCard from '@/components/Product/ProductCard';
 import { Product } from '@/types/Product';
 
@@ -11,7 +12,7 @@ const ProductList: FC<Props> = ({ products }) => {
   return (
     <SimpleGrid cols={3} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard key={product._id} product={product} />
       ))}
     </SimpleGrid>
   );
