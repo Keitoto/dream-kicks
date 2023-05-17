4c48388a502886858bfedb4ea1e0fbcb663db5be
Keitoto <keitasekihara@gmail.com>
2023-05-17 18:14:43 +0900

    ✨ added axios interceptor for request

--- frontend/src/components/apiClient.ts
+++ frontend/src/components/apiClient.ts
@@ -7,3 +7,14 @@ export const apiClient = axios.create({
     'Content-Type': 'application/json',
   },
 });
+apiClient.interceptors.request.use(
+  async (config) => {
+    if (localStorage.getItem('userInfo')) {
+      config.headers.Authorization = `Bearer ${
+        JSON.parse(localStorage.getItem('userInfo')!).token
+      }`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
