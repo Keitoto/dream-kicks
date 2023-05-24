8ce1184c921d017fa5fbef634f257195a2db45c7
Keitoto <keitasekihara@gmail.com>
2023-05-24 19:03:11 +0900

    ♻️ Changed file to folder with index

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,16 +1,12 @@
 import { Helmet } from 'react-helmet-async';
-import {
-  BackgroundImage,
-  Container,
-  Title,
-} from '@mantine/core';
+import { Container } from '@mantine/core';
 
-import heroPc from '@/assets/hero_pc.jpg';
 import LoadingBox from '@/components/LoadingBox';
 import MessageBox from '@/components/MessageBox';
 import { ProductList } from '@/components/Product/ProductList';
 import { SectionHeading } from '@/components/UI/SectionHeading';
 import { useGetProductsQuery } from '@/hooks/productHooks';
+import { HeroSection } from '@/pages/HomePage/HeroSection';
 
 export const HomePage = () => {
   const { data: products, error, isLoading } = useGetProductsQuery();
@@ -40,26 +36,15 @@ export const HomePage = () => {
         <title>Dream Kicks</title>
         <meta name="description" content="Home page for Dream Kicks" />
       </Helmet>
-      <BackgroundImage src={heroPc} h="calc(100vh - 64px)">
-        <Container
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          <Title order={1}>Welcome to Dream Kicks</Title>
-        </Container>
-      </BackgroundImage>
+      <HeroSection />
       <Container mt="xl">
         <section className="mt-24">
           <SectionHeading order={2}>New Arrivals</SectionHeading>
-          <ProductList products={products.slice(0,4)} className="mt-12" />
+          <ProductList products={products.slice(0, 8)} className="mt-12" />
         </section>
         <section className="mt-24">
           <SectionHeading order={2}>Best Seller</SectionHeading>
-          <ProductList products={products.slice(8,12)} className="mt-12" />
+          <ProductList products={products.slice(8, 12)} className="mt-12" />
         </section>
       </Container>
     </>
