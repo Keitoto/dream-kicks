71c37c0ef82d436dd81168d165ac3c59fc0d586c
Keitoto <keitasekihara@gmail.com>
2023-05-24 20:25:14 +0900

    ✨ Added guest login

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
 
+import { Sign } from 'crypto';
+import { toast } from 'react-toastify';
+import { Basket, ChevronDown, User } from 'tabler-icons-react';
 import {
-  Basket,
-  ChevronDown,
-} from 'tabler-icons-react';
-import { Badge, Button, Container, Flex, Menu, Text } from '@mantine/core';
+  ActionIcon,
+  Badge,
+  Button,
+  Container,
+  Flex,
+  Menu,
+  Text,
+} from '@mantine/core';
 
+import { StyledLink } from '@/components/UI/StyledLink';
 import { HEADER_HEIGHT } from '@/constants/styles';
+import { useSignInMutation } from '@/hooks/userHooks';
 import { useAppDispatch, useAppSelector } from '@/store';
 import { selectCartItems } from '@/store/cartSlice';
-import { selectUserInfo, signOut } from '@/store/userSlice';
+import { selectUserInfo, signIn, signOut } from '@/store/userSlice';
 
 export const Header = () => {
   const cartItems = useAppSelector(selectCartItems);
@@ -25,6 +34,21 @@ export const Header = () => {
     window.location.href = '/signin';
   };
 
+  const { mutateAsync: signInMutation, isLoading } = useSignInMutation();
+
+  const handleSignIn = async (email: string, password: string) => {
+    try {
+      const userInfo = await signInMutation({ email, password });
+      dispatch(signIn(userInfo));
+      localStorage.setItem('userInfo', JSON.stringify(userInfo));
+      window.location.reload();
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    }
+  };
+
   return (
     <Container size="lg">
       <Flex
@@ -35,35 +59,56 @@ export const Header = () => {
         px="md"
       >
         <Link to="/" className="no-underline font-bold">
-          <Text size="xl" color="teal">
-            Dream Kicks
-          </Text>
+          <Text className="text-default text-2xl">Dream Kicks</Text>
         </Link>
         <nav>
           <Flex gap="md" align="center">
-            {userInfo ? (
-              <Menu>
-                <Menu.Target>
-                  <Button rightIcon={<ChevronDown size={20} />}>
-                    {userInfo.name}
-                  </Button>
-                </Menu.Target>
+            <Menu trigger="hover">
+              <Menu.Target>
+                <ActionIcon className="text-default">
+                  <User />
+                </ActionIcon>
+              </Menu.Target>
+              {userInfo ? (
                 <Menu.Dropdown>
+                  <Menu.Label>
+                    <Text size="xs">{userInfo.name}</Text>
+                  </Menu.Label>
+                  <Menu.Divider />
                   <Menu.Item>
-                    <Link to="/orders">Order History</Link>
+                    <StyledLink to="/orders">Order History</StyledLink>
                   </Menu.Item>
-                  <Menu.Item>
-                    <Link to="/signout" onClick={signOutHandler}>
-                      Sign out
-                    </Link>
+                  <Menu.Item
+                    onClick={signOutHandler}
+                    className="hover:text-hover"
+                  >
+                    Sign out
                   </Menu.Item>
                 </Menu.Dropdown>
-              </Menu>
-            ) : (
-              <Link to="/signin">Sign in</Link>
-            )}
+              ) : (
+                <Menu.Dropdown>
+                  <Menu.Item>
+                    <Link to="/signin">Sign in</Link>
+                  </Menu.Item>
+                  <Menu.Divider />
+                  <Menu.Item
+                    onClick={() => handleSignIn('admin@example.com', '123456')}
+                  >
+                    Test as Admin
+                  </Menu.Item>
+                  <Menu.Item
+                    onClick={() =>
+                      handleSignIn('customer@example.com', '123456')
+                    }
+                  >
+                    Test as Customer
+                  </Menu.Item>
+                </Menu.Dropdown>
+              )}
+            </Menu>
+
             <Link to="/cart" className="flex items-center gap-1">
-              <Basket color="black" />
+              <Basket className="text-default" />
               {cartItems.length > 0 && (
                 <Badge variant="light" color="teal">
                   {cartItems.reduce((acc, item) => acc + item.quantity, 0)}
