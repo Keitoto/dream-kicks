e5e13471b52240c268a6ffda7c4d54da0cc04175
Keitoto <keitasekihara@gmail.com>
2023-05-24 20:23:50 +0900

    💄 Different layout for admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
 import { Outlet } from 'react-router-dom';
+
 import { ToastContainer } from 'react-toastify';
 import { AppShell } from '@mantine/core';
 
 import 'react-toastify/dist/ReactToastify.css';
+import { useAppSelector } from '@/store';
+import { selectIsAdmin } from '@/store/userSlice';
+
 import { Header, Footer } from './components/Layout';
 
 const App = () => {
+  const isAdmin = useAppSelector(selectIsAdmin);
   return (
     <>
-      <AppShell padding={0} header={<Header />} footer={<Footer />}>
-        <Outlet />
-      </AppShell>
+      {isAdmin ? (
+        // Admin dashboard
+        <AppShell padding={0} header={<Header />}>
+          <Outlet />
+        </AppShell>
+      ) : (
+        // Normal user
+        <AppShell padding={0} header={<Header />} footer={<Footer />}>
+          <Outlet />
+        </AppShell>
+      )}
       <ToastContainer position="bottom-center" limit={1} />
     </>
   );
