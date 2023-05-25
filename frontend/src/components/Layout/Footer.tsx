a2b5bbb556dc7d596d1e076bda439241230621fc
Keitoto <keitasekihara@gmail.com>
2023-05-25 17:16:28 +0900

    ✨ Added footer content

--- frontend/src/components/Layout/Footer.tsx
+++ frontend/src/components/Layout/Footer.tsx
@@ -1,3 +1,118 @@
+import { Text, Container, Flex, Stack, List, Box, Group } from '@mantine/core';
+import {
+  Mail,
+  MapPin,
+  Phone,
+  BrandFacebook,
+  BrandInstagram,
+  BrandTwitter,
+  BrandPinterest,
+} from 'tabler-icons-react';
+
+import { useGetProductsQuery } from '@/hooks/productHooks';
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  const { data: products, error, isLoading } = useGetProductsQuery();
+
+  return (
+    <>
+      <footer className="bg-[#ECEBEC] px-8 py-16">
+        <Container size="xl">
+          <Flex gap="xl" justify="space-between">
+            <Stack mr="xl">
+              <Text className="font-bold text-2xl">Dream Kicks</Text>
+              <List spacing="sm" mt="md" c="gray.6">
+                <List.Item icon={<MapPin />}>1234, Berlin, Germany</List.Item>
+                <List.Item icon={<Mail />}>info@example.com</List.Item>
+                <List.Item icon={<Phone />}>+001 1234 5678</List.Item>
+              </List>
+              <Flex gap="md" c="gray.7">
+                <BrandFacebook />
+                <BrandInstagram />
+                <BrandTwitter />
+                <BrandPinterest />
+              </Flex>
+            </Stack>
+            <Flex
+              ml="xl"
+              gap="40px"
+              justify="space-between"
+              className="flex-1 max-w-[500px]"
+            >
+              <Box>
+                <Text mb="xl">Categories</Text>
+                {isLoading ? (
+                  <Text>Loading...</Text>
+                ) : !products || products.length === 0 ? (
+                  <Text>No category found</Text>
+                ) : (
+                  <List listStyleType="none" c="gray.6" spacing="sm">
+                    {[
+                      ...new Set(
+                        products.flatMap((product) => product.category)
+                      ),
+                    ]
+                      .sort()
+                      .map((category) => (
+                        <List.Item>{category}</List.Item>
+                      ))}
+                  </List>
+                )}
+              </Box>
+              <Box>
+                <Text mb="xl">Brand</Text>
+                {isLoading ? (
+                  <Text>Loading...</Text>
+                ) : !products || products.length === 0 ? (
+                  <Text>No category found</Text>
+                ) : (
+                  <List listStyleType="none" c="gray.6" spacing="sm">
+                    {[
+                      ...new Set(
+                        products.flatMap((product) => product.category)
+                      ),
+                    ]
+                      .sort()
+                      .map((category) => (
+                        <List.Item>{category}</List.Item>
+                      ))}
+                  </List>
+                )}
+              </Box>
+              <Box>
+                <Text mb="xl">Information</Text>
+                <List listStyleType="none" c="gray.6" spacing="sm">
+                  <List.Item>About us</List.Item>
+                  <List.Item>Contact us</List.Item>
+                  <List.Item>Terms & Conditions</List.Item>
+                  <List.Item>Returns & Exchanges</List.Item>
+                  <List.Item>Shipping & Delivery</List.Item>
+                  <List.Item>Privacy Policy</List.Item>
+                </List>
+              </Box>
+            </Flex>
+            <Box className="max-w-[300px]">
+              <Text mb="xl">Newsletter</Text>
+              <Text c="gray.6">
+                Subscribe to our newsletter and get 10% off your first purchase
+              </Text>
+              <Group mt="md" spacing="sm">
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+                />
+                <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600">
+                  Subscribe
+                </button>
+              </Group>
+            </Box>
+          </Flex>
+        </Container>
+      </footer>
+      <Box className="text-center bg-white p-3 text-xs">
+        <Text c="gray.6">© 2021 Dream Kicks. All rights reserved.</Text>
+      </Box>
+    </>
+  );
 };
