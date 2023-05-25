6329a2214a088bc7ab3a6ae4a29959c0a37daf48
Keitoto <keitasekihara@gmail.com>
2023-05-25 16:10:24 +0900

    ✨ Added sidebar for admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { AppShell, Navbar, Space } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
-
 import { ToastContainer } from 'react-toastify';
-import { AppShell } from '@mantine/core';
 
 import 'react-toastify/dist/ReactToastify.css';
 import { useAppSelector } from '@/store';
+
+import { HEADER_HEIGHT } from '@/constants/styles';
+import { Sidebar } from '@/components/Layout/Sidebar';
 import { selectIsAdmin } from '@/store/userSlice';
 
 import { Header, Footer } from './components/Layout';
@@ -15,12 +17,25 @@ const App = () => {
     <>
       {isAdmin ? (
         // Admin dashboard
-        <AppShell padding={0} header={<Header />}>
+        <AppShell
+          padding={0}
+          header={<Header />}
+          aside={
+            <Navbar
+              hiddenBreakpoint="sm"
+              width={{ lg: 160, sm: 100 }}
+              height="100%"
+            >
+              <Sidebar />
+            </Navbar>
+          }
+        >
           <Outlet />
         </AppShell>
       ) : (
         // Normal user
         <AppShell padding={0} header={<Header />} footer={<Footer />}>
+          <Space h={HEADER_HEIGHT} />
           <Outlet />
         </AppShell>
       )}
